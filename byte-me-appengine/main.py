1a79ef8bcf249368422a181f0dc2d89ec293553f
Valerie Fanelle <vfanelle@gmail.com>
2016-07-25 11:22:42 -0400

    "Updating main.py"

--- byte-me-appengine/main.py
+++ byte-me-appengine/main.py
@@ -14,23 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-<<<<<<< HEAD
-import webapp2
-import jinja2
-import os
-
-template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_environment = jinja2.Environment(
-    loader=jinja2.FileSystemLoader(template_dir)
-)
-
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello world!')
-
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-=======
 from google.appengine.ext import ndb
 import jinja2
 import logging
@@ -52,27 +35,21 @@ jinja_environment = jinja2.Environment(
 #handler for main page
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Welcome to Induviae!')
-        # template = jinja_environment.get_template('main.html')
-        # self.response.write(template.render())
+        template = jinja_environment.get_template('input.html')
+        self.response.write(template.render())
 
-
-    # def post(self):
-        #template = jinja_environment.get_template('input.html')
-        #how do I transition the main page to the input page? Do I need a separate handler?
-        #self.response.write(template.render())
-        # user_zip = self.request.get('zip_code') #change info in quotes to correspond to input.html input variables
-        # user_country = self.request.get('country_code')
-        # user_gender = self.request.get('gender')
-        # user_temp = self.request.get('average_feel')
-        # user = User(zip_code=user_zip, country_code=user_country, gender=user_gener, body_temp=user_temp)
-        # key = user.put()
-
-
+    
 class OutputHandler(webapp2.RequestHandler):
-    def get(self):
-        # template = jinja_environment.get_template('output.html')
-        # self.response.write(template.render({explanation}))
+    def post(self):
+        user_zip = self.request.get('zip_code') #change info in quotes to correspond to input.html input variables
+        user_country = self.request.get('country_code')
+        user_gender =   self.request.get('gender')
+        user_temp = self.request.get('average_feel')
+        user = User(zip_code=user_zip, country_code=user_country, gender=user_gener, body_temp=user_temp)
+        key = user.put()
+
+        template = jinja_environment.get_template('output.html')
+        self.response.write(template.render({explanation}))
 
         #setting up the weather api, used meme example
         #API key = d18790d3f622ff63af5b3fc8902387db
@@ -80,17 +57,16 @@ class OutputHandler(webapp2.RequestHandler):
         response = urllib2.urlopen(req)
         response_text = response.read()
         response_data = json.loads(response_text)
-        outside_avg_temp = response_data['main']['temp'])
+        outside_avg_temp = response_data['main']['temp']
         outside_humidity = response_data['main']['humidity']
         outside_min_temp = response_data['main']['temp_min']
         outside_max_temp = response_data['main']['temp_max']
         wind_speed = response_data['wind']['speed']
 
-        # template = jinja_environment.get_template('output.html')
-        # html = template.render({'output_weather_img':weather_img})
+        template = jinja_environment.get_template('output.html')
+        html = template.render({'output_weather_img':weather_img})
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/outfit', OutputHandler)
->>>>>>> dc55d159d17910bd6a0789587d94d5b755087a68
 ], debug=True)
