cd1737f88b98419d80d42d9e8f79fdd722222581
Valerie Fanelle <vfanelle@gmail.com>
2016-07-27 17:13:38 -0400

    "Put more clothing into temp_datastore.py"

--- byte-me-appengine/temp_datastore.py
+++ byte-me-appengine/temp_datastore.py
@@ -16,42 +16,103 @@ jinja_environment = jinja2.Environment(
 class TempDatastoreHandler(webapp2.RequestHandler):
     def get(self):
         clothing_list= [
+
+        outfits.Clothing(article="shirt", name="Long-sleeve Sweater", temp=40,
+        gender="male", url="http://static.hellyhansen.com/img/hh/catalog/large/51719_949_main_large.png"),
+        outfits.Clothing(article="shirt", name="Long Sleeve T-shirt", temp=50,
+        gender="male", url="https://image.spreadshirtmedia.com/image-server/v1/productTypes/23/views/1/appearances/4,width=378,height=378/Men-s-Long-Sleeve-T-Shirt.png"),
+        outfits.Clothing(article="shirt", name="Button down", temp=60,
+        gender="male", url="https://www.riversendtrading.com/prod/WAMHigh/1514235_1.png"),
+        outfits.Clothing(article="shirt", name="Button down", temp=70,
+        gender="male", url="http://sierrasummitgear.com/product_images/q/127/S12-Peakline-Shirt-SS-Riptide__58630.png"),
         outfits.Clothing(article="shirt", name="T-Shirt", temp=80,
         gender="male", url="https://image.spreadshirtmedia.net/image-server/v1/productTypes/6/views/1/appearances/4,width=378,height=378/Men-s-T-Shirt.png"),
-        outfits.Clothing(article="shirt", name="Button down", temp=75,
-        gender="male", url="http://sierrasummitgear.com/product_images/q/127/S12-Peakline-Shirt-SS-Riptide__58630.png"),
+        outfits.Clothing(article="shirt", name="T-Shirt", temp=90,
+        gender="male", url="https://image.spreadshirtmedia.net/image-server/v1/productTypes/6/views/1/appearances/4,width=378,height=378/Men-s-T-Shirt.png"),
+        outfits.Clothing(article="shirt", name="T-Shirt", temp=100,
+        gender="male", url="https://image.spreadshirtmedia.net/image-server/v1/productTypes/6/views/1/appearances/4,width=378,height=378/Men-s-T-Shirt.png"),
+        outfits.Clothing(article="shirt", name="T-Shirt", temp=110,
+        gender="male", url="https://image.spreadshirtmedia.net/image-server/v1/productTypes/6/views/1/appearances/4,width=378,height=378/Men-s-T-Shirt.png"),
 
-
-        outfits.Clothing(article="bottom", name="Jeans", temp=60,
+        outfits.Clothing(article="bottom", name="Jeans", temp=40,
+        gender="male", url="http://cdn.shopify.com/s/files/1/0284/2118/products/20031_F.png?v=1389386884"),
+        outfits.Clothing(article="bottom", name="Jeans", temp=50,
         gender="male", url="http://pngimg.com/upload/jeans_PNG5778.png"),
         outfits.Clothing(article="bottom", name="Jeans", temp=60,
         gender="male", url="http://pngimg.com/upload/jeans_PNG5745.png"),
-        outfits.Clothing(article="bottom", name="Shorts", temp=82,
+        outfits.Clothing(article="bottom", name="Shorts", temp=70,
+        gender="male", url="http://www.jjsoftwear.com/wp-content/uploads/2011/10/2.png"),
+        outfits.Clothing(article="bottom", name="Shorts", temp=80,
+        gender="male", url="http://www.jjsoftwear.com/wp-content/uploads/2011/10/2.png"),
+        outfits.Clothing(article="bottom", name="Shorts", temp=90,
+        gender="male", url="http://www.jjsoftwear.com/wp-content/uploads/2011/10/2.png"),
+        outfits.Clothing(article="bottom", name="Shorts", temp=100,
+        gender="male", url="http://www.jjsoftwear.com/wp-content/uploads/2011/10/2.png"),
+        outfits.Clothing(article="bottom", name="Shorts", temp=110,
         gender="male", url="http://www.jjsoftwear.com/wp-content/uploads/2011/10/2.png"),
 
 
-        outfits.Clothing(article="shoes", name="Vans", temp=60,
-        gender="male", url="https://cdn1.thehunt.com/app/public/system/note_images/1255271/original/83af6749c33b4eaddbb0e996db9c9503.png"),
         outfits.Clothing(article="shoes", name="Vans", temp=40,
         gender="male", url="http://images.footlocker.com/pi/34274001/zoom/jordan-retro-2-mens"),
-        outfits.Clothing(article="shoes", name="Vans", temp=92,
-        gender="male", url="http://idboardshop.com/media/catalog/product/cache/1/image/510x/602f0fa2c1f0d1ba5e241f914e856ff9/0/7/077916.png"),
-        outfits.Clothing(article="shoes", name="Sandals", temp=95,
+        outfits.Clothing(article="shoes", name="Vans", temp=50,
+        gender="male", url="http://www.merkatoexpress.com/media/wysiwyg/superstore/product-1.png"),
+        outfits.Clothing(article="shoes", name="Vans", temp=60,
+        gender="male", url="https://cdn1.thehunt.com/app/public/system/note_images/1255271/original/83af6749c33b4eaddbb0e996db9c9503.png"),
+        outfits.Clothing(article="shoes", name="Vans", temp=70,
+        gender="male", url="http://images.nike.com/is/image/DotCom/144587C_001_A?$AFI$"),
+        outfits.Clothing(article="shoes", name="Sandals", temp=80,
         gender="male", url="http://images.lacrosseunlimited.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/R/a/Rainbow_Sandals_Premier_Leathers_dark_brown_mocha_301ALTS0_dkbr_42.png"),
-        outfits.Clothing(article="shoes", name="Sandals", temp=82,
+        outfits.Clothing(article="shoes", name="Vans", temp=90,
+        gender="male", url="http://idboardshop.com/media/catalog/product/cache/1/image/510x/602f0fa2c1f0d1ba5e241f914e856ff9/0/7/077916.png"),
+        outfits.Clothing(article="shoes", name="Sandals", temp=100,
+        gender="male", url="http://images.lacrosseunlimited.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/R/a/Rainbow_Sandals_Premier_Leathers_dark_brown_mocha_301ALTS0_dkbr_42.png"),
+        outfits.Clothing(article="shoes", name="Sandals", temp=110,
         gender="male", url="http://images.lacrosseunlimited.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/R/a/Rainbow_Sandals_Premier_Leathers_dark_brown_mocha_301ALTS0_dkbr_42.png"),
 
 
         outfits.Clothing(article="shirt", name="V-neck T-shirt", temp=75,
         gender="female", url="http://images.footlocker.com/pi/84683100/large/nike-shortsleeve-legend-v-neck-t-shirt-2.0-womens"),
 
-
-        outfits.Clothing(article="bottom", name="Skinny Jeans", temp=75,
+        outfits.Clothing(article="bottom", name="Skinny Jeans", temp=40,
         gender="female", url="http://www.jeans.ch/out/pictures/master/product/1/sol_nudie-jeans-skinny-lin-jeans-women-light-blue-skinny-lin-111818_f_1.png"),
+        outfits.Clothing(article="bottom", name="Skinny Jeans", temp=50,
+        gender="female", url="http://www.jeans.ch/out/pictures/master/product/1/sol_nudie-jeans-skinny-lin-jeans-women-light-blue-skinny-lin-111818_f_1.png"),
+        outfits.Clothing(article="bottom", name="Skinny Jeans", temp=60,
+        gender="female", url="http://www.jeans.ch/out/pictures/master/product/1/sol_nudie-jeans-skinny-lin-jeans-women-light-blue-skinny-lin-111818_f_1.png"),
+        outfits.Clothing(article="bottom", name="Skinny Jeans", temp=70,
+        gender="female", url="http://www.jeans.ch/out/pictures/master/product/1/sol_nudie-jeans-skinny-lin-jeans-women-light-blue-skinny-lin-111818_f_1.png"),
+        outfits.Clothing(article="bottom", name="Shorts", temp=80,
+        gender="female", url="https://wakeeffects.com/wp-content/uploads/2015/06/pool-375x374.png"),
+        outfits.Clothing(article="bottom", name="Shorts", temp=90,
+        gender="female", url="https://wakeeffects.com/wp-content/uploads/2015/06/pool-375x374.png"),
+        outfits.Clothing(article="bottom", name="Shorts", temp=100,
+        gender="female", url="https://wakeeffects.com/wp-content/uploads/2015/06/pool-375x374.png"),
+        outfits.Clothing(article="bottom", name="Shorts", temp=110,
+        gender="female", url="https://wakeeffects.com/wp-content/uploads/2015/06/pool-375x374.png"),
 
 
-        outfits.Clothing(article="shoes", name="Vans", temp=75,
+        outfits.Clothing(article="shoes", name="Uggs", temp=40,
+        gender="female", url="http://31.sqrbear.com/wp-content/uploads/2014/05/UGG-Bailey-Button-Triplet-chestnut-Womens.png"),
+        outfits.Clothing(article="shoes", name="Uggs", temp=50,
+        gender="female", url="http://31.sqrbear.com/wp-content/uploads/2014/05/UGG-Bailey-Button-Triplet-chestnut-Womens.png"),
+        outfits.Clothing(article="shoes", name="Vans", temp=60,
+        gender="female", url="http://essentiallymom.com/wp-content/uploads/2014/02/Womens-Flat-Rain-Boots.png"),
+        outfits.Clothing(article="shoes", name="Vans", temp=70,
         gender="female", url="http://www.gliks.com/Assets/ProductImages/M9166_shot1.png"),
+        outfits.Clothing(article="shoes", name="Flats", temp=80,
+        gender="female", url="https://i6.govx.io/images/136895_olukai_womens_pueo_womens_pueo_bronzekona_coffee_5_t300.png?v=GiE4OnoXIVuE8cfm5KlU2w=="),
+        outfits.Clothing(article="shoes", name="Sandals", temp=90,
+        gender="female", url="http://cdn.shopify.com/s/files/1/0219/5414/products/SIDE_BAXLEY2_PLATINUM_GOLD_METALLIC_NAPPA_large.png?v=1451439229"),
+        outfits.Clothing(article="shoes", name="Sandals", temp=100,
+        gender="female", url="http://cdn.shopify.com/s/files/1/0219/5414/products/SIDE_BAXLEY2_PLATINUM_GOLD_METALLIC_NAPPA_large.png?v=1451439229"),
+        outfits.Clothing(article="shoes", name="Sandals", temp=110,
+        gender="female", url="http://cdn.shopify.com/s/files/1/0219/5414/products/SIDE_BAXLEY2_PLATINUM_GOLD_METALLIC_NAPPA_large.png?v=1451439229"),
+
+
+
+
+
+
         ]
         for clothing in clothing_list:
           num_matching = outfits.Clothing.query().filter(outfits.Clothing.url == clothing.url).count()
